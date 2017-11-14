e56ac560bfceed6f4aa3a7b654a323698227c0ac
Mateusz Nowak <kontakt.mateusznowak@gmail.com>
2017-11-15 00:28:46 +0100

    Lista2 Dodanie rekursywnej metody wypisujacej szefów

--- lista1/src/lista2_zad18-28.sql
+++ lista1/src/lista2_zad18-28.sql
@@ -120,14 +120,111 @@ BEGIN
   END LOOP;
 END;
 
+---ZADANIE 21
+DECLARE
+  BEGIN
+
+END;
+
+
 ---ZADANIE 23
 CREATE OR REPLACE TRIGGER porzadek_w_papierach_band
   BEFORE INSERT ON bandy
-  
+  FOR EACH ROW
+  BEGIN
+    SELECT NVL(MAX(bandy.nr_bandy),0) + 1 INTO :new.nr_bandy FROM bandy;
+  END;
+
+SET AUTOCOMMIT OFF;
+
+INSERT INTO bandy VALUES (23,'OSZOŁOMY','KUCHNIA',NULL);
+
+SELECT nr_bandy FROM Bandy WHERE nazwa='OSZOŁOMY';
+
+ROLLBACK;
+SET AUTOCOMMIT ON;
+
+
+
 
 
 DECLARE
   liczba_przelozonych NUMBER:= &ile_przelozonych;
 BEGIN
+  FOR kocur_lizus IN ( SELECT * FROM kocury WHERE funkcja IN ('KOT','MILUSIA'))
+  LOOP
+    DBMS_OUTPUT.PUT_LINE('Kocur lizus: ' || kocur_lizus.imie);
+  END LOOP;
+END;
 
+CREATE OR REPLACE PROCEDURE wypisz_szefow(kocur kocury%ROWTYPE)
+  IS
+    szef_kocura kocury%ROWTYPE;
+  BEGIN
+    IF kocur.szef IS NULL THEN
+      RETURN;
+      ELSE
+        SELECT * INTO szef_kocura FROM kocury WHERE kocury.pseudo = kocur.szef;
+        DBMS_OUTPUT.PUT_LINE('Szef: ' || szef_kocura.imie);
+        wypisz_szefow(szef_kocura);
+    END IF;
+  END;
+
+  DECLARE
+    kot kocury%ROWTYPE;
+  BEGIN
+    SELECT * INTO kot FROM kocury WHERE kocury.imie = 'LUCEK';
+    wypisz_szefow(kot);
+  END;
+
+
+
+DECLARE
+  curr_level NUMBER DEFAULT 1;
+  max_level NUMBER DEFAULT 0;
+  n_level NUMBER DEFAULT &n;
+  kot kocury%ROWTYPE;
+BEGIN
+
+  SELECT
+    MAX(level)-1 INTO max_level
+  FROM Kocury
+  CONNECT BY PRIOR pseudo = szef
+  START WITH szef IS NULL;
+
+  IF n_level > max_level THEN
+    n_level := max_level;
+  END IF;
+
+  DBMS_OUTPUT.PUT(RPAD('Imie', 10));
+
+  FOR i IN 1..n_level LOOP
+    DBMS_OUTPUT.PUT('  |  ' || RPAD('Szef ' || i, 10));
+  END LOOP;
+
+  DBMS_OUTPUT.PUT_LINE(' ');
+  DBMS_OUTPUT.PUT('----------');
+  FOR i IN 1..n_level LOOP
+    DBMS_OUTPUT.PUT(' --- ----------');
+  END LOOP;
+  DBMS_OUTPUT.PUT_LINE(' ');
+
+  FOR rekord IN (
+  SELECT * FROM Kocury
+  WHERE funkcja IN ('KOT', 'MILUSIA')
+  ) LOOP
+    curr_level := 1;
+    DBMS_OUTPUT.PUT(RPAD(rekord.imie, 10));
+    kot := rekord;
+    WHILE curr_level <= n_level LOOP
+      IF kot.szef IS NULL THEN
+        DBMS_OUTPUT.PUT('  |  ' || RPAD(' ', 10));
+      ELSE
+        SELECT * INTO kot FROM Kocury WHERE pseudo=kot.szef;
+        DBMS_OUTPUT.put('  |  ' || RPAD(kot.imie, 10));
+      END IF;
+      curr_level := curr_level + 1;
+    END LOOP;
+    DBMS_OUTPUT.PUT_LINE(' ');
+  END LOOP;
 END;
