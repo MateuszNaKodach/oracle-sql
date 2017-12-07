780321b8b489b4ac0effcf0900c23424f8f61258
Mateusz Nowak <kontakt.mateusznowak@gmail.com>
2017-12-07 23:28:44 +0100

    Lista3 Add data for all 3 tables

--- lista1/src/lista3-tabele&dane.sql
+++ lista1/src/lista3-tabele&dane.sql
@@ -32,7 +32,30 @@ CONSTRAINT obj_sludzyelit_sluga_nn CHECK (sluga IS NOT NULL),
 CONSTRAINT obj_sludzyelit_pan_nn CHECK (pan IS NOT NULL)
 );
 
+---TRIGGER NA TYLKO JEDNEGO SLUGE DLA ELITY
+--ALTER SESSION SET PLSQL_WARNINGS = 'ENABLE:ALL';
 
+CREATE OR REPLACE TRIGGER jeden_sluga_dla_elity
+  BEFORE INSERT OR UPDATE ON objsludzyelit
+  FOR EACH ROW
+  DECLARE
+    ile_slug INTEGER;
+    nadmiar_slug EXCEPTION;
+    BEGIN
+
+    SELECT COUNT(*) INTO ile_slug
+    FROM objkocury K
+    WHERE K.pseudo =DEREF(:NEW.pan).pseudo;
+
+    IF(ile_slug>1)
+      THEN
+      RAISE nadmiar_slug;
+    END IF;
+
+  END;
+
+--select * from user_errors;
+---WYPELNIENIE TABELI objkocury-----------------------------------------------------------------------------------------
 INSERT INTO objkocury VALUES (elita('MRUCZEK', 'M', 'TYGRYS', NULL, '2002-01-01', 103, 33));
 INSERT INTO objkocury VALUES (elita('RUDA', 'D', 'MALA', (SELECT REF(k)
                                                           FROM objkocury k
@@ -55,7 +78,6 @@ INSERT INTO objkocury VALUES (elita('BOLEK', 'M', 'LYSY', (SELECT REF(k)
 INSERT INTO objkocury VALUES (plebs('KSAWERY', 'M', 'MAN', (SELECT REF(k)
                                                             FROM objkocury k
                                                             WHERE k.pseudo = 'RAFA'), '2008-07-12', 51, NULL));
-
 INSERT INTO objkocury VALUES (plebs('MELA', 'D', 'DAMA', (SELECT REF(k)
                                                           FROM objkocury k
                                                           WHERE k.pseudo = 'RAFA'), '2008-11-01', 51, NULL));
@@ -88,9 +110,121 @@ INSERT INTO objkocury VALUES (plebs('LUCEK', 'M', 'ZERO', (SELECT REF(k)
                                                            WHERE k.pseudo = 'KURKA'), '2010-03-01', 43, NULL));
 COMMIT;
 
+---WYPELNIENIE TABELI objsludzyelit-----------------------------------------------------------------------------------------
+---FUNKCJA TREAT: http://psoug.org/definition/TREAT.htm
+INSERT INTO objsludzyelit VALUES (
+  1,
+  (SELECT TREAT((SELECT REF(p)
+                 FROM objkocury p
+                 WHERE p.pseudo = 'TYGRYS') AS REF ELITA)
+   FROM dual),
+  (SELECT TREAT((SELECT REF(p)
+                 FROM objkocury p
+                 WHERE p.pseudo = 'DAMA') AS REF PLEBS)
+   FROM dual)
+);
 
+INSERT INTO objsludzyelit VALUES (
+  2,
+  (SELECT TREAT((SELECT REF(p)
+                 FROM objkocury p
+                 WHERE p.pseudo = 'MALA') AS REF ELITA)
+   FROM dual),
+  (SELECT TREAT((SELECT REF(p)
+                 FROM objkocury p
+                 WHERE p.pseudo = 'MAN') AS REF PLEBS)
+   FROM dual)
+);
 
+INSERT INTO objsludzyelit VALUES (
+  3,
+  (SELECT TREAT((SELECT REF(p)
+                 FROM objkocury p
+                 WHERE p.pseudo = 'LOLA') AS REF ELITA)
+   FROM dual),
+  (SELECT TREAT((SELECT REF(p)
+                 FROM objkocury p
+                 WHERE p.pseudo = 'UCHO') AS REF PLEBS)
+   FROM dual)
+);
 
+INSERT INTO objsludzyelit VALUES (
+  4,
+  (SELECT TREAT((SELECT REF(p)
+                 FROM objkocury p
+                 WHERE p.pseudo = 'RAFA') AS REF ELITA)
+   FROM dual),
+  (SELECT TREAT((SELECT REF(p)
+                 FROM objkocury p
+                 WHERE p.pseudo = 'MALY') AS REF PLEBS)
+   FROM dual)
+);
+
+COMMIT;
+
+---WSZYSCY SLUDZY ELIT:
+
+SELECT DEREF(s.pan).pseudo "Pan",
+  DEREF(s.sluga).pseudo "Sługa"
+FROM objsludzyelit s;
+
+---WYPELNIENIE TABELI objincydenty-----------------------------------------------------------------------------------------
+INSERT INTO objincydenty VALUES(1,(SELECT REF(k)
+                                   FROM objkocury k
+                                   WHERE k.pseudo = 'TYGRYS'),'KAZIO','2004-10-13','USILOWAL NABIC NA WIDLY');
+INSERT INTO objincydenty VALUES(2,(SELECT REF(k)
+                                   FROM objkocury k
+                                   WHERE k.pseudo = 'ZOMBI'),'SWAWOLNY DYZIO','2005-03-07','WYBIL OKO Z PROCY');
+INSERT INTO objincydenty VALUES(3,(SELECT REF(k)
+                                   FROM objkocury k
+                                   WHERE k.pseudo = 'BOLEK'),'KAZIO','2005-03-29','POSZCZUL BURKIEM');
+INSERT INTO objincydenty VALUES(4,(SELECT REF(k)
+                                   FROM objkocury k
+                                   WHERE k.pseudo = 'SZYBKA'),'GLUPIA ZOSKA', '2006-09-12', 'UZYLA KOTA JAKO SCIERKI');
+INSERT INTO objincydenty VALUES(5,(SELECT REF(k)
+                                   FROM objkocury k
+                                   WHERE k.pseudo = 'MALA'),'CHYTRUSEK', '2007-03-07', 'ZALECAL SIE');
+INSERT INTO objincydenty VALUES(6,(SELECT REF(k)
+                                   FROM objkocury k
+                                   WHERE k.pseudo = 'TYGRYS'),'DZIKI BILL', '2007-06-12', 'USILOWAL POZBAWIC ZYCIA');
+INSERT INTO objincydenty VALUES(7,(SELECT REF(k)
+                                   FROM objkocury k
+                                   WHERE k.pseudo = 'BOLEK'),'DZIKI BILL', '2007-11-10', 'ODGRYZL UCHO');
+INSERT INTO objincydenty VALUES(8,(SELECT REF(k)
+                                   FROM objkocury k
+                                   WHERE k.pseudo = 'LASKA'),'DZIKI BILL', '2008-12-12', 'POGRYZL ZE LEDWO SIE WYLIZALA');
+INSERT INTO objincydenty VALUES(9,(SELECT REF(k)
+                                   FROM objkocury k
+                                   WHERE k.pseudo = 'LASKA'),'KAZIO', '2009-01-07', 'ZLAPAL ZA OGON I ZROBIL WIATRAK');
+INSERT INTO objincydenty VALUES(10,(SELECT REF(k)
+                                   FROM objkocury k
+                                   WHERE k.pseudo = 'DAMA'),'KAZIO', '2009-02-07', 'CHCIAL OBEDRZEC ZE SKORY');
+INSERT INTO objincydenty VALUES(11,(SELECT REF(k)
+                                    FROM objkocury k
+                                    WHERE k.pseudo = 'MAN'), 'REKSIO', '2009-04-14', 'WYJATKOWO NIEGRZECZNIE OBSZCZEKAL');
+INSERT INTO objincydenty VALUES(12,(SELECT REF(k)
+                                    FROM objkocury k
+                                    WHERE k.pseudo = 'LYSY'), 'BETHOVEN', '2009-05-11', 'NIE PODZIELIL SIE SWOJA KASZA');
+INSERT INTO objincydenty VALUES(13,(SELECT REF(k)
+                                    FROM objkocury k
+                                    WHERE k.pseudo = 'RURA'), 'DZIKI BILL','2009-09-03', 'ODGRYZL OGON');
+INSERT INTO objincydenty VALUES(14,(SELECT REF(k)
+                                    FROM objkocury k
+                                    WHERE k.pseudo = 'PLACEK'), 'BAZYLI', '2010-07-12', 'DZIOBIAC UNIEMOZLIWIL PODEBRANIE KURCZAKA');
+INSERT INTO objincydenty VALUES(15,(SELECT REF(k)
+                                    FROM objkocury k
+                                    WHERE k.pseudo = 'PUSZYSTA'), 'SMUKLA', '2010-11-19', 'OBRZUCILA SZYSZKAMI');
+INSERT INTO objincydenty VALUES(16,(SELECT REF(k)
+                                    FROM objkocury k
+                                    WHERE k.pseudo = 'KURKA'), 'BUREK', '2010-12-14', 'POGONIL');
+INSERT INTO objincydenty VALUES(17,(SELECT REF(k)
+                                    FROM objkocury k
+                                    WHERE k.pseudo = 'MALY'), 'CHYTRUSEK', '2011-07-13', 'PODEBRAL PODEBRANE JAJKA');
+INSERT INTO objincydenty VALUES(18,(SELECT REF(k)
+                                    FROM objkocury k
+                                    WHERE k.pseudo = 'UCHO'), 'SWAWOLNY DYZIO', '2011-07-14', 'OBRZUCIL KAMIENIAMI');
+
+COMMIT;
 
 
 
@@ -102,23 +236,10 @@ COMMIT;
 ---INSERT INTO objkocury VALUES(plebs('BARI', 'M', 'RURA', (SELECT REF(k) FROM objkocury k WHERE k.pseudo='TYGRYS'), '2009-09-01', 56, NULL))
 COMMIT;
 
----FUNKCJA TREAT: http://psoug.org/definition/TREAT.htm
-INSERT INTO objsludzyelit VALUES (
-  (SELECT TREAT((SELECT REF(p)
-                 FROM objkocury p
-                 WHERE p.pseudo = 'TYGRYS') AS REF ELITA)
-   FROM dual),
-  (SELECT TREAT((SELECT REF(p)
-                 FROM objkocury p
-                 WHERE p.pseudo = 'LOLA') AS REF PLEBS)
-   FROM dual)
-);
-COMMIT;
 
 SELECT value(p)
 FROM objkocury p
 WHERE VALUE(p) IS OF (ELITA);
-COMMIT;
 
 
 SELECT TREAT(value(k) AS ELITA)
